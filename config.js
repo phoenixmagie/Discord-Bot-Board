28479ce87c24385f40611b6817ed5711459c28e6
phoenixmagie <phoenixmagie.mc@gmail.com>
2026-04-21 19:38:21 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const BOT_CONFIG = {
+    // Passwort für den Zugriff
+    password: "VIP-System", 
+    
+    // Bot Daten
+    clientKey: "Sdv944HGdH9M0NbCCu4g4Tp7FFkdStho",
+    oauthUrl: "https://discord.com/oauth2/authorize?client_id=1461675860598718477&permissions=8&integration_type=0&scope=bot",
+    
+    // System Info
+    fileName: "CLAN_DATA"
+};
